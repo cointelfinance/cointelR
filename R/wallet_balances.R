11ac623227c6fcf3349e7ebbb254051328648808
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-02-24 18:08:20 -0500

    updating to query wallet

--- R/wallet_balances.R
+++ R/wallet_balances.R
@@ -5,6 +5,10 @@
 #' @param wallet_address Path to the input file
 #' @return API call for wallet balances
 #' @export
-wallet_balance_check <- function(wallet_address){
+#'
 
+wallet_balance_check <- function(wallet_address){
+  wallet_url <- paste0("https://cointel-api.herokuapp.com/wallet_transactions?wallet_address=",wallet_address)
+  wallet_data <- jsonlite::fromJSON(wallet_url)
+  return(wallet_data)
 }
