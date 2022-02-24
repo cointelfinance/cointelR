556f38f2bcda82d7ab58da1ea67882282517d99e
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2022-02-24 18:14:03 -0500

    updating function

--- R/wallet_balances.R
+++ R/wallet_balances.R
@@ -2,11 +2,10 @@
 #'
 #' This pings the API via R to query blockchain balances
 #'
-#' @param wallet_address Path to the input file
-#' @return API call for wallet balances
+#' @param wallet_address Wallet Address to Check
+#' @return Wallet Balances across blockchains
 #' @export
 #'
-library(jsonlite)
 wallet_balance_check <- function(wallet_address){
   wallet_url <- paste0("https://cointel-api.herokuapp.com/wallet_transactions?wallet_address=",wallet_address)
   wallet_data <- jsonlite::fromJSON(wallet_url)
